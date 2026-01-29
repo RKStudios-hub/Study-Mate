b1e141d9206d05c84e018450dededf5e29a5d790
user <user@example.com>
2026-01-29 23:59:39 +0530

    feat: Update app icon (user instructions) and loading animation

--- src/app/components/FileViewerModal.tsx
+++ src/app/components/FileViewerModal.tsx
@@ -56,17 +56,13 @@ export function FileViewerModal({ isOpen, onClose, file, fileDataUrl }: FileView
     if (!file || !fileDataUrl) {
       return (
         <div className="p-4 text-center text-gray-700">
-          <div class="loader">
-            <figure class="iconLoaderProgress">
-              <svg class="iconLoaderProgressFirst" width="240" height="240">
-                <circle cx="120" cy="120" r="100"></circle>
-              </svg>
-
-              <svg class="iconLoaderProgressSecond" width="240" height="240">
-                <circle cx="120" cy="120" r="100"></circle>
-              </svg>
-            </figure>
-          </div>
+          <svg style="left: 50%;
+        top: 50%;
+        position: absolute;
+        transform: translate(-50%, -50%) matrix(1, 0, 0, 1, 0, 0);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 187.3 93.7" height="300px" width="400px">
+      <path d="M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 				c-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" stroke-width="4" fill="none" id="outline" stroke="#4E4FEB"></path>
+      <path d="M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 				c-8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" stroke-width="4" stroke="#4E4FEB" fill="none" opacity="0.05" id="outline-bg"></path>
+    </svg>
         </div>
       );
     }
